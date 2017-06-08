e3b0d1ad3bc0fb018c93d4ba334afa6d809660e6
aawesh <shrestha.aawesh@gmail.com>
2017-06-08 13:32:40 -0500

    - Main layout stabilized - Handled message correctly

--- app/src/main/java/wifipeertopeer/com/wifipeertopeer/MainActivity.java
+++ app/src/main/java/wifipeertopeer/com/wifipeertopeer/MainActivity.java
@@ -30,10 +30,6 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity implements SalutDataCallback, View.OnClickListener{
 
-    /*
-        This simple activity demonstrates how to use the Salut library from a host and client perspective.
-     */
-
     public static final String TAG = "SalutTestApp";
     public SalutDataReceiver dataReceiver;
     public SalutServiceData serviceData;
@@ -43,7 +39,7 @@ public class MainActivity extends AppCompatActivity implements SalutDataCallback
     public TextView tv1,tv2,tv3;
     int id = 0;
 
-    SalutDataCallback callback;
+    private boolean hostCreated = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -65,11 +61,9 @@ public class MainActivity extends AppCompatActivity implements SalutDataCallback
         with some instantiated object from our app. */
         dataReceiver = new SalutDataReceiver(this, this);
 
-//        id = new Random().nextInt(10)+65;
-        id = 8;
+        id = new Random().nextInt(10)+65;
         /*Populate the details for our awesome service. */
-        serviceData = new SalutServiceData("testAwesomeService", 60606,
-                "DEVICE"+id);
+        serviceData = new SalutServiceData("PedestrianService", 8888, "DEVICE_"+id);
 
         /*Create an instance of the Salut class, with all of the necessary data from before.
         * We'll also provide a callback just in case a device doesn't support WiFi Direct, which
@@ -77,7 +71,7 @@ public class MainActivity extends AppCompatActivity implements SalutDataCallback
         network = new Salut(dataReceiver, serviceData, new SalutCallback() {
             @Override
             public void call() {
-                // wiFiFailureDiag.show();
+                // wiFiFailureDiag.show(); TODO
                 // OR
                 Log.e(TAG, "Sorry, but this device does not support WiFi Direct.");
             }
@@ -85,32 +79,35 @@ public class MainActivity extends AppCompatActivity implements SalutDataCallback
 
     }
 
-    private void setupNetwork()
-    {
-        if(!network.isRunningAsHost)
-        {
+    private void setupNetwork() {
+        if(!network.isRunningAsHost){
             network.startNetworkService(new SalutDeviceCallback() {
                 @Override
                 public void call(SalutDevice salutDevice) {
                     Toast.makeText(getApplicationContext(), "Device: " + salutDevice.instanceName + " connected.", Toast.LENGTH_LONG).show();
+                    hostCreated = true;
                 }
             });
 
-            tv1.setText("Host_name: DEVICE"+id);
-            tv2.setText("I am a HOST");
+            tv1.setText("Pedestrian DEVICE_"+id);
+            tv2.setText("Service Started");
 
             hostingBtn.setText("Stop Service");
             discoverBtn.setAlpha(0.5f);
             discoverBtn.setClickable(false);
         }
-        else
-        {
-            tv1.setText("No HOST");
-            tv1.setText("No HOST");
+        else {
+            tv1.setText("Pedestrian");
+            tv2.setText("Service Stoppped");
+            tv3.setText("");
             network.stopNetworkService(false);
             hostingBtn.setText("Start Service");
             discoverBtn.setAlpha(1f);
             discoverBtn.setClickable(true);
+            if(hostCreated) {
+                network.stopNetworkService(true);
+                hostCreated = false;
+            }
         }
     }
 
@@ -118,24 +115,29 @@ public class MainActivity extends AppCompatActivity implements SalutDataCallback
     {
         if(!network.isRunningAsHost && !network.isDiscovering)
         {
+            tv2.setText("Started Dsicovering");
             network.discoverNetworkServices(new SalutCallback() {
                 @Override
                 public void call() {
                     Toast.makeText(getApplicationContext(), "Device: " + network.foundDevices.get(0).instanceName + " found. ", Toast.LENGTH_SHORT).show();
-                    tv1.setText("Client: found"+network.foundDevices.get(0).instanceName+ " and size: "+network.foundDevices.size() );
+
+                    tv1.setText("Driver");
+                    tv2.setText("Host " + network.foundDevices.get(0).deviceName + " found");
+
+                    //TODO remove debug
                     for(int i = 0;i<network.foundDevices.size();i++){
-                        Log.d("Aawesh====",network.foundDevices.get(i).instanceName);
-                        Log.d("Aawesh====",network.foundDevices.get(i).instanceName);
+                        Log.d("Host_"+i+1,network.foundDevices.get(i).instanceName);
                     }
 
+                    //for now registered with the first host. Don't know what happens of there are multipe host
                     network.registerWithHost(network.foundDevices.get(0), new SalutCallback() {
                         @Override
                         public void call() {
                             Log.d(TAG, "We're now registered.");
-                            tv2.setText("Registered with host: "+network.foundDevices.get(0));
+                            tv2.setText("Registered with host: "+network.foundDevices.get(0).deviceName);
                             //send message
                             Message myMessage = new Message();
-                            myMessage.description = "Hello host !!!"+ " from: " + network.thisDevice;
+                            myMessage.description = "Hello pedestrian !!!"+ " from driver: " + network.thisDevice.deviceName;
 
                             network.sendToHost(myMessage, new SalutCallback() {
                                 @Override
@@ -143,7 +145,7 @@ public class MainActivity extends AppCompatActivity implements SalutDataCallback
                                     Log.e(TAG, "Oh no! The data failed to send.");
                                 }
                             });
-                            tv3.setText("Message sent from Client: "+myMessage.description);
+//                            tv3.setText("Message sent from Client: "+myMessage.description);
                         }
                     }, new SalutCallback() {
                         @Override
@@ -159,10 +161,16 @@ public class MainActivity extends AppCompatActivity implements SalutDataCallback
         }
         else
         {
-            network.stopServiceDiscovery(true);
+            if(!network.isRunningAsHost){
+                network.stopServiceDiscovery(true);
+            }
             discoverBtn.setText("Discover Services");
             hostingBtn.setAlpha(1f);
             hostingBtn.setClickable(false);
+
+            tv1.setText("Driver");
+            tv2.setText("Stopped Discovering");
+            tv3.setText("");
         }
     }
 
@@ -182,7 +190,7 @@ public class MainActivity extends AppCompatActivity implements SalutDataCallback
         {
             Message newMessage = LoganSquare.parse(o.toString(), Message.class);
             Log.d(TAG, newMessage.description);  //See you on the other side!
-            tv3.setText("Message received: "+newMessage.description);
+            tv3.setText(newMessage.description);
             //Do other stuff with data.
         }
         catch (IOException ex)
@@ -193,7 +201,7 @@ public class MainActivity extends AppCompatActivity implements SalutDataCallback
         //this means host has recieved at least one message from clients which means that at least one client is registered with host
         if(network.isRunningAsHost){ // if ishost or issome boolean true for first receipt from client then host can start sending data regardless of any message received from the clients TODO
             Message myMessage = new Message();
-            myMessage.description = "Hello clients !!! from host "+network.thisDevice;
+            myMessage.description = "Hello driver !!! from pedestrian: "+network.thisDevice.deviceName;
             Log.d(TAG,myMessage.description);
 
             network.sendToAllDevices(myMessage, new SalutCallback() {
